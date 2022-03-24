65c94e265d530d603b83c128480763ca36388606
gram <mail@orsinium.dev>
2022-03-24 13:53:15 +0100

    meta

---
+++ benchmark_imports/__init__.py
@@ -0,0 +1,7 @@
+"""A CLI tool to record how much time it takes to import each dependency.
+"""
+
+from ._imports import activate, deactivate
+
+__version__ = '1.0.0'
+__all__ = ['activate', 'deactivate']
